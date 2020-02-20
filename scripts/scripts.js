826305b496d3bb4dc842eb4e43d1000a9f9d5a70
KarimTazmi <57253656+KarimTazmi@users.noreply.github.com>
2020-02-20 16:25:57 +0100

    Added mobile code

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+function backgroundColorChange() {
+  const link = document.querySelector(".showcase__link");
+  const background = document.querySelector(".showcase");
+
+  link.onmouseover function() {
+      background.backgroundColor("#545454");} 
+}
